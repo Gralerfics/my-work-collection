3ae9fc528b8f26ca18178e0a4288d4166ec22ef7
Gralerfics <glverfer@outlook.com>
2026-03-22 18:33:33 +0100

    fix(pages): add repository name to base when built by Actions.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const isGitHubPagesBuild = process.env.GITHUB_ACTIONS === 'true'
+
 export default defineConfig({
   plugins: [vue()],
+  base: isGitHubPagesBuild && repositoryName ? `/${repositoryName}/` : '/',
 })
